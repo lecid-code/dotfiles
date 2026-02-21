418a2dee06d26c54cb4ca8e0edd7cb0dc7168133
Ramzi Sayyid <code@lecid.me>
2026-02-21 15:33:28 -0600

    home-manager: migrate git.nix

--- .config/home-manager/home.nix
+++ .config/home-manager/home.nix
@@ -15,27 +15,10 @@
   # release notes.
   home.stateVersion = "25.11"; # Please read the comment before changing.
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  home.packages = with pkgs; [
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
-    gh
-    glab
+  imports = [
+    ./modules/packages.nix
+    ./modules/shell.nix
+    ./modules/git.nix
   ];
 
   # Home Manager is pretty good at managing dotfiles. The primary way to manage
@@ -53,52 +36,8 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/rsayyid/etc/profile.d/hm-session-vars.sh
-  #
-  home.sessionVariables = {
-    EDITOR = "nvim";
-    VISUAL = "nvim";
-    GOPATH = "$HOME/.go";
-    LESS = "-RSM~gIsw";
-    LC_ALL = "en_US.UTF-8";
-    LC_CTYPE = "en_US.UTF-8";
-    FZF_DEFAULT_OPTS = "-m --bind='ctrl-o:execute(nvim {})+abort'";
-    FZF_DEFAULT_COMMAND = "rg --files";
-  };
-
-  home.sessionPath = [
-    "$HOME/.go/bin"
-    "$HOME/.local/bin"
-    "$HOME/bin"
-    "$HOME/.bun/bin"
-  ];
-
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  programs.fish = {
-    enable = true;
-    interactiveShellInit = ''
-      /home/rsayyid/.local/bin/mise activate fish | source
-    '';
-    loginShellInit = ''
-      source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-    '';
-  };
 
   programs.neovim = {
     enable = true;
