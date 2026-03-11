232fe05dbc4fec1770bb4068049f113144f45ca3
Ramzi Sayyid <code@lecid.me>
2026-03-11 14:21:49 -0500

    remove integrated status line between tmux and nvim - too crowded

--- .config/nvim/lua/custom/plugins/init.lua
+++ .config/nvim/lua/custom/plugins/init.lua
@@ -30,12 +30,12 @@ return {
 		},
 	},
 
-	{
-		"vimpostor/vim-tpipeline",
-		config = function()
-			vim.g.tpipeline_autoembed = 1
-			vim.g.tpipeline_restore = 1
-			vim.g.tpipeline_clearstl = 1
-		end,
-	},
+	-- {
+	-- 	"vimpostor/vim-tpipeline",
+	-- 	config = function()
+	-- 		vim.g.tpipeline_autoembed = 1
+	-- 		vim.g.tpipeline_restore = 1
+	-- 		vim.g.tpipeline_clearstl = 1
+	-- 	end,
+	-- },
 }
