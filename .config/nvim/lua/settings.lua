be324eaff421472fd420d16d827540459917f5ce
Ramzi Sayyid <code@lecid.me>
2026-03-24 06:58:44 -0500

    nvim: Format buffer on save

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -145,3 +145,11 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function() vim.hl.on_yank() end,
 })
+
+-- Format buffer on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = "*",
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
