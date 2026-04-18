5de451728fc0511adfc26354cdb98a0acc9386a4
Ramzi Sayyid <code@lecid.me>
2026-04-18 15:43:07 +0000

    fish: Fix Go env variables

--- .config/fish/conf.d/00-path.fish
+++
@@ -1,4 +0,0 @@
-# Add mise to PATH
-fish_add_path --path $HOME/.bun/bin
-fish_add_path --path $HOME/.local/bin
-fish_add_path --path $HOME/.go/bin
--- .config/fish/conf.d/terminal.fish
+++ .config/fish/conf.d/terminal.fish
@@ -1,6 +1,8 @@
 if status is-interactive
     set -q fish_key_bindings; or set -g fish_key_bindings fish_default_key_bindings
 
+    fish_add_path --path $HOME/.local/bin
+
     command -q mise && mise activate fish | source
     command -q fzf && fzf --fish | source
     command -q zoxide && zoxide init fish | source
